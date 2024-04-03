685397cb47a11fce504810c926b318c3a1b1fb2f
James Othniel <jamesothniel7@gmail.com>
2024-04-03 18:42:45 +0200

    added assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
-import { VscSettingsGear } from "react-icons/vsc";
-import MainSide from "./components/MainSide";
-import Sidebar, { SidebarItems } from "./components/Sidebar";
-import { AiFillApple } from "react-icons/ai";
+const App = () => {
+  return <div>App</div>;
+};
 
-export default function App() {
-  return (
-    <>
-      <div className="flex">
-        {/* <Sidebar /> */}
-        <Sidebar>
-          <SidebarItems
-            icon={<AiFillApple size={20} />}
-            text="AiFillAliwangwang"
-          />
-          <SidebarItems
-            icon={<VscSettingsGear size={20} />}
-            text="AiFillAliwangwang"
-          />
-        </Sidebar>
-        <MainSide />
-      </div>
-    </>
-  );
-}
+export default App;
