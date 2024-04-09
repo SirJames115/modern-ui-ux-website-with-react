c3ce1f7c51fd315d174123d92ac56d334edfa116
James Othniel <jamesothniel7@gmail.com>
2024-04-09 12:24:03 +0200

    complete the hero section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -10,12 +10,13 @@ const Section = ({
 }) => {
   return (
     <div
-    id={id}
-      className={`relative ${
+      id={id}
+      className={`relative 
+      ${
         customPaddings ||
-        `py-10 lg:py-16 xl:py20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${
-          className || ""
-        } `
+        `py-10 lg:py-16 xl:py-20 
+        ${crosses ? "lg:py-32 xl:py-40" : ""} 
+        ${className || ""} `
       }`}>
       {children}
       <div className="hidden absolute top-0 left-5 w-0.25 h-full bg-stroke-1 pointer-events-none md:block lg:left-7.5 xl:left-10" />
@@ -28,7 +29,7 @@ const Section = ({
               crossOffset && crossOffset
             } pointer-events-none lg:block xl:left-10 right-10`}
           />
-          <SectionSvg crossesOffset={crossOffset}/>
+          <SectionSvg crossesOffset={crossOffset} />
         </>
       )}
     </div>
